ea2f70b38d3cd66faf954f81ec64300f027140b4
Kyle Gough <kylegough98@gmail.com>
2019-11-30 04:17:11 +0000

    Refactor, Jellyfish error, Swordfish possible error.

--- solver.py
+++ solver.py
@@ -156,8 +156,8 @@ def h2(g):
     g, success = sectorSetCover(g)
     return g, success
 
-# Detects an X-Wing in the grid.
-def xwing(g, k):
+# Detects an X-Wing/Swordfish/Jellyfish in the grid.
+def xsj(g, k):
     candidates = []
     candidateValues = []
 
@@ -178,7 +178,7 @@ def xwing(g, k):
             for j in allOccurences:
                 if (j[0] == i):
                     count += 1
-            if (count == k):
+            if (count == k):###
                 values.add(i)
         # Value/Row Pair.
         a = [a for a in allOccurences if a[0] in values]
@@ -186,7 +186,7 @@ def xwing(g, k):
         candidates.append(a)
         candidateValues.append(values)
     
-    # Identify which values can occur in the X-Wing.
+    # Identify which values can occur in the X-Wing/Swordfish/Jellyfish.
     for i in range(1,10):
         count = 0
         # Count if a value occurs k times in a column, for at least k rows.
@@ -194,33 +194,33 @@ def xwing(g, k):
             if (i in candidateValues[a]):         
                 count += 1
         # If restricted in at least k columns.
-        if (count >= k):
+        if (count >= k):###
             rowList = []
             for a in range(g.size):
                 rowList.append([c[1] for c in candidates[a] if c[0] == i])
 
-            # Coordinates of the X-Wing.
+            # Coordinates of the X-Wing/Swordfish/Jellyfish.
             cols = []
             rows = []
 
             # Detects occurences across columns.
             for p in range(g.size):
-                if (len(rowList[p]) == k):
-                    if (rowList.count(rowList[p]) == k):
+                if (len(rowList[p]) == k):###
+                    if (rowList.count(rowList[p]) == k):###
                         cols.append(p)
                         rows = rowList[p]
             
-            # Detects an X-Wing.
-            if (len(cols) == k and len(rows) == k):
+            # Detects an X-Wing/Swordfish/Jellyfish.
+            if (len(cols) == k and len(rows) == k):###
                 g.log(1, getStructureName(k) + " of value " + str(i) + " found at rows:" + str(rows) + " cols:" + str(cols))
-                g, success =  xwingSolve(g, k, i, rows, cols)
+                g, success =  xsjSolve(g, k, i, rows, cols)
                 if (success):
                     return g, success
                 
     return g, False
 
-# Reduces valid values for cells conflicting with an X-Wing.
-def xwingSolve(g, k, n, rows, cols):
+# Reduces valid values for cells conflicting with an X-Wing/Swordfish/Jellyfish.
+def xsjSolve(g, k, n, rows, cols):
     success = False
     for x in range(g.size):
         for y in rows:
@@ -276,7 +276,7 @@ def strategicSolver(g):
             return g, False
 
         # X-Wing along columns.
-        g, found = xwing(g, 2)
+        g, found = xsj(g, 2)
         if (found):
             continue
         if (g.error):
@@ -285,7 +285,7 @@ def strategicSolver(g):
 
         #X-Wing along rows.
         g.transpose()
-        g, found = xwing(g, 2)
+        g, found = xsj(g, 2)
         g.transpose()
         if (found):
             continue
@@ -294,7 +294,7 @@ def strategicSolver(g):
             return g, False
        
         # Swordfish along columns.
-        g, found = xwing(g, 3)
+        g, found = xsj(g, 3)
         if (found):
             continue
         if (g.error):
@@ -303,7 +303,7 @@ def strategicSolver(g):
 
         # Swordfish along rows.
         g.transpose()
-        g, found = xwing(g, 3)
+        g, found = xsj(g, 3)
         g.transpose()
         if (found):
             continue
@@ -312,7 +312,7 @@ def strategicSolver(g):
             return g, False
 
         # Jellyfish along columns.
-        g, found = xwing(g, 4)
+        g, found = xsj(g, 4)
         if (found):
             continue
         if (g.error):
@@ -321,7 +321,7 @@ def strategicSolver(g):
 
         # Jellyfish along rows.
         g.transpose()
-        g, found = xwing(g, 4)
+        g, found = xsj(g, 4)
         g.transpose()
         if (found):
             continue
@@ -342,7 +342,7 @@ if __name__ == "__main__":
     g = difficultGridTest()
     g = xwingGridTest()
     g = swordfishGridTest()
-    #g = jellyfishGridTest()
+    g = jellyfishGridTest()
 
     # Command Line arguments.
     print("Command Line Arguments")
@@ -362,3 +362,7 @@ if __name__ == "__main__":
     print()
     print("[" + tCol.OKGREEN + " SOLUTION " + tCol.ENDC + "]")
     g.printClean()
+    
+    ###
+    ###print()
+    ###g.printValid()
