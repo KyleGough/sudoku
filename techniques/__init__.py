6a572a7832afc3a190e26785a76151e8e5ad86d9
Kyle Gough <kylegough98@gmail.com>
2019-12-05 02:49:53 +0000

    Techniques cache.

--- techniques/__init__.py
+++ techniques/__init__.py
@@ -3,3 +3,4 @@ from techniques.hiddenCandidate import hiddenCandidate
 from techniques.subsetCover import subsetCover
 from techniques.xsj import xwing, swordfish, jellyfish, getStructureName
 from techniques.pointingPairs import pointingPairs
+from techniques.boxLineReduction import boxLineReduction
